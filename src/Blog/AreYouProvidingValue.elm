bd7ddd63475571632480be93fa8b4200546a2eb5
matthew-matvei <matthew.d.james87@gmail.com>
2020-06-21 15:09:54 +0100

    Create outline of page

--- src/Blog/AreYouProvidingValue.elm
+++ src/Blog/AreYouProvidingValue.elm
@@ -3,8 +3,51 @@ module Blog.AreYouProvidingValue exposing (getContent)
 import Blog.Content exposing (Content(..))
 import Blog.ProgrammingAsASecondLanguage exposing (getContent)
 
+
 getContent : () -> List Content
 getContent _ =
     [ Title "Are you providing value?"
     , SubTitle "... and how the road to hell is paved with good intentions"
-    , WhenCreated "June 21 2020" ]
+    , WhenCreated "June 21 2020"
+    , Section
+        { title =
+            Just
+                { title = "Good ideas"
+                , subTitle = "...that don't work"
+                }
+        , content =
+            []
+        }
+    , Section
+        { title =
+            Just
+                { title = "Principles over practice"
+                , subTitle = ""
+                }
+        , content = []
+        }
+    , Section
+        { title =
+            Just
+                { title = "Helper libraries"
+                , subTitle = ""
+                }
+        , content = []
+        }
+    , Section
+        { title =
+            Just
+                { title = "Tests"
+                , subTitle = "More = better?"
+                }
+        , content = []
+        }
+    , Section
+        { title =
+            Just
+                { title = "YAGNI"
+                , subTitle = "You ain't gonna need it (probably)"
+                }
+        , content = []
+        }
+    ]
