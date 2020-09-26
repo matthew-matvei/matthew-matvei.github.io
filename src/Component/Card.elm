af7b5b2f12f72907e9d9b1c4cf1ff1fea3954e49
matthew-matvei <matthew.d.james87@gmail.com>
2020-09-27 00:07:02 +0100

    Dump idea

--- src/Component/Card.elm
+++ src/Component/Card.elm
@@ -1,7 +1,7 @@
 module Component.Card exposing (readArticleAction, view)
 
-import Html exposing (Html, a, div, p, span, text)
-import Html.Attributes exposing (class, href)
+import Html exposing (Html, a, div, img, p, span, text)
+import Html.Attributes exposing (class, href, src, style)
 import Widget exposing (externalLink)
 
 
@@ -16,33 +16,62 @@ type alias Model =
     { title : String
     , text : String
     , actions : List Action
+    , backgroundImage : Maybe String
     }
 
 
 view : Model -> Html msg
 view model =
-    div [ class "card purple hoverable" ]
-        [ div [ class "card-content white-text" ]
-            [ span [ class "card-title" ]
-                [ text model.title
-                ]
-            , p [] [ text model.text ]
-            ]
-        , div [ class "card-action purple darken-3" ]
-            (List.map
-                viewAction
-                model.actions
-            )
+    div
+        [ class "card small purple hoverable"
         ]
+        (List.append (viewCardContent model) [ viewActions model.actions ])
+
+
+viewCardContent : Model -> List (Html msg)
+viewCardContent model =
+    case model.backgroundImage of
+        Just imageUrl ->
+            [ div [ class "card-image" ] [ img [ src imageUrl ] [] ]
+            , div
+                [ class "card-content white-text" ]
+                [ span [ class "card-title" ] [ text model.title ]
+                , p [] [ text model.text ]
+                ]
+            ]
+
+        Nothing ->
+            [ div [ class "card-content white-text" ]
+                [ div [ class "card-title" ] [ text model.title ]
+                , p [] [ text model.text ]
+                ]
+            ]
+
+
+viewActions : List Action -> Html msg
+viewActions actions =
+    div [ class "card-action purple darken-3" ] (List.map viewAction actions)
 
 
 viewAction : Action -> Html msg
 viewAction action =
-    if action.linkIsExternal
-    then externalLink [ href action.link ] [ text action.text ]
-    else a [ href action.link ] [ text action.text ]
+    if action.linkIsExternal then
+        externalLink [ href action.link ] [ text action.text ]
+
+    else
+        a [ href action.link ] [ text action.text ]
 
 
 readArticleAction : String -> Action
 readArticleAction link =
     { link = link, linkIsExternal = False, text = "Read" }
+
+
+backgroundImage : Maybe String -> List (Html msg)
+backgroundImage maybeUrl =
+    case maybeUrl of
+        Just url ->
+            [ img [ src url, style "position" "absolute", style "left" "0", style "top" "0", style "opacity" "0.5", style "width" "100%" ] [] ]
+
+        Nothing ->
+            []
--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -10,63 +10,73 @@ view _ =
     main_ [ class "container" ]
         (viewApps () ++ viewBlogArticles ())
 
+
 viewApps : () -> List (Html msg)
 viewApps _ =
     [ h2 [] [ text "Apps" ]
     , div [ class "row" ]
-    [ div [ class "col s12 m6 l6" ][
-        Card.view {
-            title = "Terminal Tetris"
-            , text = "... a simple game of Tetris in the terminal"
-            , actions = [{
-                link = "https://github.com/matthew-matvei/TerminalTetris"
-                , linkIsExternal = True
-                , text = "Play"
-            }]
-        }
-    ]]]
+        [ div [ class "col s12 m6 l6" ]
+            [ Card.view
+                { title = "Terminal Tetris"
+                , text = "... a simple game of Tetris in the terminal"
+                , actions =
+                    [ { link = "https://github.com/matthew-matvei/TerminalTetris"
+                      , linkIsExternal = True
+                      , text = "Play"
+                      }
+                    ]
+                , backgroundImage = Just "/assets/img/tetris-230x230.png"
+                }
+            ]
+        ]
+    ]
+
 
 viewBlogArticles : () -> List (Html msg)
 viewBlogArticles _ =
     [ h2 [] [ text "Tech musings" ]
-        , div [ class "row" ]
-            [ div [ class "col s12 m6 l6" ]
-                [ Card.view
-                    { title = "3 Best Practices in programming"
-                    , text = "... and how there are no best practices"
-                    , actions =
-                        [ Card.readArticleAction "/blog/no-best-practices"
-                        ]
-                    }
-                ]
-            , div [ class "col s12 m6 l6" ]
-                [ Card.view
-                    { title = "Programming as a Second Language"
-                    , text = "... and why I'm glad I've learnt / taught a second language"
-                    , actions =
-                        [ Card.readArticleAction "/blog/programming-as-a-second-language"
-                        ]
-                    }
-                ]
+    , div [ class "row" ]
+        [ div [ class "col s12 m6 l6" ]
+            [ Card.view
+                { title = "3 Best Practices in programming"
+                , text = "... and how there are no best practices"
+                , actions =
+                    [ Card.readArticleAction "/blog/no-best-practices"
+                    ]
+                , backgroundImage = Nothing
+                }
             ]
-        , div [ class "row" ]
-            [ div [ class "col s12 m6 l6" ]
-                [ Card.view
-                    { title = "Are you providing value?"
-                    , text = "... and how the road to hell is paved with good intentions"
-                    , actions =
-                        [ Card.readArticleAction "/blog/are-you-providing-value"
-                        ]
-                    }
-                ]
-            , div [ class "col s12 m6 l6" ]
-                [ Card.view
-                    { title = "The Either / Result pattern"
-                    , text = "... for network calls"
-                    , actions =
-                        [ Card.readArticleAction "/blog/either-pattern-for-network-calls"
-                        ]
-                    }
-                ]
+        , div [ class "col s12 m6 l6" ]
+            [ Card.view
+                { title = "Programming as a Second Language"
+                , text = "... and why I'm glad I've learnt / taught a second language"
+                , actions =
+                    [ Card.readArticleAction "/blog/programming-as-a-second-language"
+                    ]
+                , backgroundImage = Nothing
+                }
             ]
         ]
+    , div [ class "row" ]
+        [ div [ class "col s12 m6 l6" ]
+            [ Card.view
+                { title = "Are you providing value?"
+                , text = "... and how the road to hell is paved with good intentions"
+                , actions =
+                    [ Card.readArticleAction "/blog/are-you-providing-value"
+                    ]
+                , backgroundImage = Nothing
+                }
+            ]
+        , div [ class "col s12 m6 l6" ]
+            [ Card.view
+                { title = "The Either / Result pattern"
+                , text = "... for network calls"
+                , actions =
+                    [ Card.readArticleAction "/blog/either-pattern-for-network-calls"
+                    ]
+                , backgroundImage = Nothing
+                }
+            ]
+        ]
+    ]
